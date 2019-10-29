3e6d7c3f54c01da9b620f67a0017cc55e6e58ac0
Sam Lee <cable8mm@gmail.com>
2019-10-30 01:00:25 +0900

    deco

--- init.php
+++ init.php
@@ -2,39 +2,35 @@
 
 namespace Cable8mm\PhpunitStartKit;
 
-if (count($argv) == 1) {
+if (\count($argv) == 1) {
     echo "error - need argv. ex) type \"php init.php Cable8mm\\\\Library\"\n";
     exit;
 }
 
-$namespace = $argv[1];
+$namespace = $argv[1];  // eg. Cable8mm\\Library
 
 echo '1. replace composer.json';
 
-$composer = json_decode(file_get_contents('composer.json'), true);
+$composer = \json_decode(\file_get_contents('composer.json'), true);
 unset($composer['autoload']['psr-4']);
 $composer['autoload']['psr-4'][$namespace . '\\'] = 'src/';
-
-\file_put_contents('composer.json', json_encode($composer, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
-
+\file_put_contents('composer.json', \json_encode($composer, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
 echo ' > done' . PHP_EOL;
 
 echo '2. replace StartKit.php';
 
 $startKit = \file_get_contents('src/StartKit.php');
-$startKit = preg_replace('/^namespace App;$/m', 'namespace ' . $namespace . ';', $startKit);
-file_put_contents('src/StartKit.php', $startKit);
-
+$startKit = \preg_replace('/^namespace App;$/m', 'namespace ' . $namespace . ';', $startKit);
+\file_put_contents('src/StartKit.php', $startKit);
 echo ' > done' . PHP_EOL;
 
 echo '3. composer install' . PHP_EOL;
 
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-    $output = popen('composer install', 'r');
+if (\strtoupper(\substr(PHP_OS, 0, 3)) === 'WIN') {
+    $output = \popen('composer install', 'r');
 } else {
-    $output = shell_exec('composer install');
+    $output = \shell_exec('composer install');
 }
+echo 'composer installed' . PHP_EOL . PHP_EOL;
 
-echo 'composer installed' . PHP_EOL;
-
-echo PHP_EOL . 'enjoy TDD' . PHP_EOL;
+echo '4. enjoy TDD' . PHP_EOL;
